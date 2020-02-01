e5bbc763ef59c54846eaa56a4313fb27887d3b3c
Ankit Singh <35894301+Mr-Perfectt@users.noreply.github.com>
2020-02-01 16:22:52 +0530

    Create pyramid.cpp

---
+++ pyramid.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	/*
+	INPUT :     5
+	
+	OUTPUT :    *
+	            **
+	            ***
+	            ****
+	            *****
+	*/
+	int n;
+	cin>>n;
+	
+	for(int i=0;i<n;i++)
+	{
+	   for(int j=n;j>i;j--)
+	        cout<<"*";
+	   cout<<endl;
+	}
+	return 0;
+}
