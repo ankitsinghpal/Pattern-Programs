b1636e69fd05faab8d6aa4ce9bd103bbb2db3833
Ankit Singh <35894301+Mr-Perfectt@users.noreply.github.com>
2020-02-01 18:19:22 +0530

    Update HalfPyramid.cpp

--- HalfPyramid.cpp
+++ HalfPyramid.cpp
@@ -15,9 +15,9 @@ int main() {
 	int n;
 	cin>>n;
 	
-	for(int i=0;i<n;i++)
+	for(int i=1;i<=n;i++)
 	{
-	   for(int j=0;j<=i;j++)
+	   for(int j=1;j<=i;j++)
 	        cout<<"*";
 	   cout<<endl;
 	}
