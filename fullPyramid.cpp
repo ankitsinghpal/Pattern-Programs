8daae6cee0611afcb06751a3855370647f586007
Ankit Singh <35894301+Mr-Perfectt@users.noreply.github.com>
2020-02-01 17:42:43 +0530

    Update fullPyramid.cpp

--- fullPyramid.cpp
+++ fullPyramid.cpp
@@ -19,7 +19,7 @@ int main() {
         for(int j = 1; j <= n-i; j++)
             cout<<"  ";
         
-        for(int k=0;k != 2 * i-1;k++)
+        for(int j=0;j != 2 * i-1;j++)
             cout << "* ";
         
         cout << endl;
