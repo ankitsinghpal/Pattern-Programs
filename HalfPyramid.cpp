3e91b48f8f13ce34ba290837153e201aeb6e7d63
Ankit Singh <35894301+Mr-Perfectt@users.noreply.github.com>
2020-02-01 17:53:29 +0530

    Update and rename pyramid.cpp to HalfPyramid.cpp

--- HalfPyramid.cpp
+++ HalfPyramid.cpp
@@ -5,7 +5,8 @@ int main() {
 	/*
 	INPUT :     5
 	
-	OUTPUT :    *
+	OUTPUT :    
+	            *
 	            **
 	            ***
 	            ****
@@ -16,7 +17,7 @@ int main() {
 	
 	for(int i=0;i<n;i++)
 	{
-	   for(int j=n;j>i;j--)
+	   for(int j=0;j<=i;j++)
 	        cout<<"*";
 	   cout<<endl;
 	}
