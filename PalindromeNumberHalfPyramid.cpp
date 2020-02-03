74b01d9a1e490e17a02346a082e4eecc8de1bb37
Ankit Singh <35894301+Mr-Perfectt@users.noreply.github.com>
2020-02-03 22:39:50 +0530

    Update PalindromeNumberHalfPyramid.cpp

--- PalindromeNumberHalfPyramid.cpp
+++ PalindromeNumberHalfPyramid.cpp
@@ -11,31 +11,20 @@ int main() {
                 12321
                 1234321
                 123454321
-                12345654321
 	*/
 	int n;
 	cin>>n;
 	
-	int count=1;
-	
+
 	for(int i=1;i<=n;i++)
-	{
-	     
-	   for(int j=1;j<i;j++)
-	   {
-	       cout<<count;
-	       count++;
-	   }
+	{	   
+	   for(int j=1;j<=i;j++)
+	        cout<<j;
 	   
-    	   for(int j=1;j<i;j++)
-    	   {
-    	       cout<<count;
-    	       count--;
-    	       
-    	   }
-           cout<<"1";	  
-	   count=1;
-	   cout<<endl;
+        for(int j=i-1;j>0;j--)
+    	    cout<<j;
+    	      
+        cout<<endl;
 	}
 	return 0;
 }
