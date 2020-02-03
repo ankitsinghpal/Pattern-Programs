39147cc802bb1a701339167a33d601618b63cb58
Ankit Singh <35894301+Mr-Perfectt@users.noreply.github.com>
2020-02-03 22:38:27 +0530

    Update PalindromeNumberFullPyramid.cpp

--- PalindromeNumberFullPyramid.cpp
+++ PalindromeNumberFullPyramid.cpp
@@ -16,30 +16,21 @@ int main() {
 	int n;
 	cin>>n;
 	
-	int count=1;
 
 	for(int i=1;i<=n;i++)
 	{
 	   for(int j=n;j>=i;j--)
-	   {
-	       cout<<" ";
-	   }
+	        cout<<" ";
 	   
-	   for(int j=1;j<i;j++)
-	   {
-	       cout<<count;
-	       count++;
-	   }
 	   
-    	   for(int j=1;j<i;j++)
-    	   {
-    	       cout<<count;
-    	       count--;
+	   for(int j=1;j<=i;j++)
+	        cout<<j;
+	   
+	   
+       	   for(int j=i-1;j>0;j--)
+    	       cout<<j;
     	      
-    	   }
-        cout<<"1";	  
-	   count=1;
-	   cout<<endl;
+       	    cout<<endl;
 	}
 	return 0;
 }
